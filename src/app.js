aa0c6b903b27f1d22015c6c697dc7ad7cec7b40b
yui0722 <takahaimasato222@gmail.com>
2016-07-12 11:36:39 +0900

    更新　背景スクロール

--- src/app.js
+++ src/app.js
@@ -1,25 +1,64 @@
 //app.js
-var gameScene = cc.Scene.extend({
-  onEnter: function() {
-
-    var gradient = cc.LayerGradient.create(cc.color(0, 0, 0, 255), cc.color(0x46, 0x82, 0xB4, 255));
-    this.addChild(gradient);
-
-    this._super();
-    gameLayer = new game();
-    gameLayer.init();
-    this.addChild(gameLayer);
-  }
-});
 
 var size;
 var mylabel;
+//背景スクロールで追加した部分
+var gameLayer;
+var background;
+var scrollSpeed = 1;
+
+var gameScene = cc.Scene.extend({
+    onEnter:function () {
+      //  var gradient = cc.LayerGradient.create(cc.color(0, 0, 0, 255), cc.color(0x46, 0x82, 0xB4, 255));
+      //  this.addChild(gradient);
+        this._super();
+        gameLayer = new game();
+        gameLayer.init();
+        this.addChild(gameLayer);
+    }
+});
+
 var game = cc.Layer.extend({
-  init: function() {
-    this._super();
-    size = cc.director.getWinSize();
-    mylabel = cc.LabelTTF.create("GO!", "Arial", "32");
-    mylabel.setPosition(size.width / 2, size.height / 2);
-    this.addChild(mylabel);
-  }
+    init:function () {
+        this._super();
+        size = cc.director.getWinSize();
+        // mylabel = cc.LabelTTF.create("GO!", "Arial", "32");
+        // mylabel.setPosition(size.width / 2, size.height / 2);
+        // this.addChild(mylabel);
+
+        //スクロールする背景スプライトをインスタンス　スクロール速度:scrollSpeed
+        background = new ScrollingBG();
+        this.addChild(background);
+        //scheduleUpdate関数は、描画の都度、update関数を呼び出す
+        this.scheduleUpdate();
+
+    },
+    update:function(dt){
+      //backgroundのscrollメソッドを呼び出す
+        background.scroll();
+
+    },
+
+});
+//スクロール移動する背景クラス
+var ScrollingBG = cc.Sprite.extend({
+    //ctorはコンストラクタ　クラスがインスタンスされたときに必ず実行される
+    ctor:function() {
+        this._super();
+        this.initWithFile(res.background_png);
+    },
+    //onEnterメソッドはスプライト描画の際に必ず呼ばれる
+    onEnter:function() {
+      //背景画像の描画開始位置 横960の画像の中心が、画面の端に設置される
+      this.setPosition(size.width,size.height /2 );
+      //  this.setPosition(480,160);
+    },
+    scroll:function(){
+      //座標を更新する
+        this.setPosition(this.getPosition().x-scrollSpeed,this.getPosition().y);
+        //画面の端に到達したら反対側の座標にする
+        if(this.getPosition().x<0){
+            this.setPosition(this.getPosition().x+480,this.getPosition().y);
+        }
+    }
 });
